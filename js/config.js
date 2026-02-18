0e2c522120ba6ee67cc3a77e0f8e6819c2f1fbb9
Tata <142669125+Dotaka123@users.noreply.github.com>
2026-02-18 09:22:13 +0300

    Fix auth form submission on Android browsers

--- js/config.js
+++ js/config.js
@@ -1,34 +1,45 @@
-const API_BASE_URL = "https://adminpanel-fj5l.onrender.com";
+var API_BASE_URL = "https://adminpanel-fj5l.onrender.com";
 
-async function apiCall(endpoint, options = {}) {
-    const token = localStorage.getItem('token');
-
-    const headers = {
-        'Content-Type': 'application/json',
-        ...(token ? { 'Authorization': `Bearer ${token}` } : {}),
-        ...options.headers
+function apiCall(endpoint, options) {
+    var requestOptions = options || {};
+    var token = localStorage.getItem('token');
+    var headers = {
+        'Content-Type': 'application/json'
     };
 
-    const config = {
-        method: options.method || 'GET',
-        headers: headers,
-        body: options.body || null
-    };
-
-    try {
-        const res = await fetch(API_BASE_URL + endpoint, config);
-
-        if (res.status === 401) {
-            logout();
-        }
-
-        const data = await res.json();
-        if (!res.ok) throw new Error(data.error || 'Erreur API');
-        return data;
-    } catch (error) {
-        console.error('API Call Error:', error);
-        throw error;
+    if (token) {
+        headers.Authorization = 'Bearer ' + token;
     }
+
+    if (requestOptions.headers) {
+        Object.keys(requestOptions.headers).forEach(function(key) {
+            headers[key] = requestOptions.headers[key];
+        });
+    }
+
+    var config = {
+        method: requestOptions.method || 'GET',
+        headers: headers,
+        body: requestOptions.body || null
+    };
+
+    return fetch(API_BASE_URL + endpoint, config)
+        .then(function(res) {
+            if (res.status === 401) {
+                logout();
+            }
+
+            return res.json().then(function(data) {
+                if (!res.ok) {
+                    throw new Error(data.error || 'Erreur API');
+                }
+                return data;
+            });
+        })
+        .catch(function(error) {
+            console.error('API Call Error:', error);
+            throw error;
+        });
 }
 
 function logout() {
